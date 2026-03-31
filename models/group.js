1d9ae5019e3c7818d59eda592209a00ba8d5df7b
Rachit Gaur <rachitgaur2k24@gmail.com>
2026-03-31 13:01:10 +0530

    Initial commit: FairShare project

---
+++ models/group.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const groupSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  owner: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User", 
+    required: true 
+  },
+  members: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User" 
+  }],
+  expenses: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Expense"   
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Group', groupSchema);
